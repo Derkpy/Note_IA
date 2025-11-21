a00b77db4e63e6d04df4c9f765cce604179f21e7
Derek Cerecedo <cerecedo02derek@gmail.com>
2025-11-20 18:11:49 -0600

    Primera parte de Note-IA

--- app/src/main/java/com/derkpy/note_ia/ui/theme/Theme.kt
+++ app/src/main/java/com/derkpy/note_ia/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.derkpy.note_ia.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
